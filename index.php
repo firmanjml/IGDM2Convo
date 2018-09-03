3374342f43c61c3361ce42d602b7c6e101b96dd4
firmanjml <37127022+firmanjml@users.noreply.github.com>
2018-09-03 20:49:01 +0800

    Fixed some div error & added option to sort

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
+    $sort = "oldest"; // oldest | newest
     $json = file_get_contents("message.json",0,null,null); 
     $result = $tmp = json_decode($json, true); // using a temp variable for testing
-    $result = array_reverse($result['conversation']); // sort the conversation from oldest to newest.
+    $result = ($sort == "oldest") ? array_reverse($result['conversation']) : $result['conversation'];
     $yourUsername = "firmanjml"; // write your igusername so it will display your messages at the right side.
 ?>
 
@@ -14,31 +15,27 @@
 <div class="container">
   
 <?php for ($i = 0; $i < count($result); $i++): ?>
-    
     <div class="Area">
         <?php  echo ($result[$i]['sender'] != $yourUsername) ?  '<div class="L">' :  '<div class="R">'?>
         <?php $img = ($result[$i]['sender'] != $yourUsername) ? "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSxU35znsBhAWQd5BouLIVtH1P4WNa0JZ_XXpyViHOIARbM2igbNgC6_kp5" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrEyVlaWx0_FK_sz86j-CnUC_pfEqw_Xq_xZUm5CMIyEI_-X2hRUpx1BHL" ?>
-        <img class="pimg" src="<?php echo $img; ?>"/>
+        <img class="pimg" src="<?php echo $img; ?>"=/>
             <div class="tooltip"><?php echo $result[$i]['sender']; ?></div>
         </div>
         <?php if (array_key_exists("media", $result[$i]) == 1): ?>
-        <div class="text <?php  echo ($result[$i]['sender'] != $yourUsername) ?  'L' :  'R'?> <?php  echo ($result[$i]['sender'] != $yourUsername) ?  '<div class="leftL">' :  '<div class="leftR">'?><img src="<?php echo $result[$i]['media'];?>"/></div>
+            <div class="text <?php  echo ($result[$i]['sender'] != $yourUsername) ?  'L leftL' :  'R leftR'?>"><img src="<?php echo $result[$i]['media'];?>"/></div>
         <?php elseif(array_key_exists("media_url", $result[$i]) == 1): ?>
-            <div class="text <?php  echo ($result[$i]['sender'] != $yourUsername) ?  'L' :  'R'?> <?php  echo ($result[$i]['sender'] != $yourUsername) ?  '<div class="leftL">' :  '<div class="leftR">'?><img src="<?php echo $result[$i]['media_url'];?>"/></div>
+            <div class="text <?php  echo ($result[$i]['sender'] != $yourUsername) ?  'L leftL' :  'R leftR'?>"><img src="<?php echo $result[$i]['media_url'];?>"/></div>
         <?php elseif(array_key_exists("media_share_url", $result[$i]) == 1): ?>
-            <div class="text <?php  echo ($result[$i]['sender'] != $yourUsername) ?  'L' :  'R'?> <?php  echo ($result[$i]['sender'] != $yourUsername) ?  '<div class="leftL">' :  '<div class="leftR">'?><img src="<?php echo $result[$i]['media_share_url'];?>"/></div>
-        <?php elseif(array_key_exists("video_call_action", $result[$i]) == 1): ?>
-            <div class="text <?php  echo ($result[$i]['sender'] != $yourUsername) ?  'L' :  'R'?> <?php  echo ($result[$i]['sender'] != $yourUsername) ?  '<div class="leftL">' :  '<div class="leftR">'?><b>Video Call</b></div>
+            <div class="text <?php  echo ($result[$i]['sender'] != $yourUsername) ?  'L leftL' :  'R leftR'?>"><img src="<?php echo $result[$i]['media_share_url'];?>"/></div>
+        <?php elseif(array_key_exists("video_call_action", $result[$i]) == 1): ?> 
+            <div class="text <?php  echo ($result[$i]['sender'] != $yourUsername) ?  'L leftL' :  'R leftR'?>"><b>Video Call</b></div>
         <?php elseif(array_key_exists("heart", $result[$i]) == 1): ?>
-            <div class="text <?php  echo ($result[$i]['sender'] != $yourUsername) ?  'L' :  'R'?> <?php  echo ($result[$i]['sender'] != $yourUsername) ?  '<div class="leftL">' :  '<div class="leftR">'?>❤️</div>
+            <div class="text <?php  echo ($result[$i]['sender'] != $yourUsername) ?  'L leftL' :  'R leftR'?>">❤️</div>
         <?php else: ?>
-            <div class="text <?php  echo ($result[$i]['sender'] != $yourUsername) ?  'L' :  'R'?> <?php  echo ($result[$i]['sender'] != $yourUsername) ?  '<div class="leftL">' :  '<div class="leftR">'?><?php echo $result[$i]['text']; ?></div>
+            <div class="text <?php  echo ($result[$i]['sender'] != $yourUsername) ?  'L leftL' :  'R leftR'?>"><?php echo $result[$i]['text']; ?></div>
         <?php endif; ?>
     </div>
 <?php endfor; ?>
-
 </div>
-
-
 </body>
 </html>
